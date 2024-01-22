16617a00fceea734bf3393eb04d93ecd7b19583c
TanuAWSTerra <tanu3212patel@gmail.com>
2024-01-22 00:25:58 +0000

    Argument or block definition required: An argument or block definition is required here.first commit 22 jan

--- ec2-sg.tf
+++ ec2-sg.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 provider "aws" {
   region     = "any region you can select"
   access_key = "Your access key"
@@ -56,3 +57,63 @@ resource "aws_security_group" "TP_security_group" {
 }
 
 
+=======
+provider "aws" {
+  region     = ""
+  access_key = ""
+  secret_key = ""
+}
+resource "aws_instance" "web" {
+  ami                    = "ami"
+  availability_zone      = ""
+  instance_type          = "t2.micro"
+  count                  = 1
+  vpc_security_group_ids = [aws_security_group.TP_security_group.id]
+}
+resource "aws_security_group" "TP_security_group" {
+  name        = "Terraform-sg"
+  description = "TP security group using Terraform"
+
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Terraform-sg"
+  }
+}
+
+
+>>>>>>> 4be92f5 (first commit 22 jan)
